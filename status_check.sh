47cbae510ebb85c5bd5cee2da3fef4aa14e41fb8
y-kzm <softietennis0212kazuma@gmail.com>
2022-10-12 19:26:15 +0900

    update

--- status_check.sh
+++ status_check.sh
@@ -1,38 +1,58 @@
 #!/bin/bash
+IF_PROVIDER=enp6s0
+IF_MGMT=enp1s0
+if [ $# != 1 ]; then
+    echo "Usage: ./${PROGRAM} [mode]"
+    echo "  mode: controller, compute"
+    exit 0;
+fi
+MODE=${1}
+
 echo -e "\e[34m===== IP Address Check =====\e[m"
-IF_PROVIDER=enp1s0
-IF_MGMT=enp6s0
 INET_PROVIDER=$(ip -f inet -o addr show ${IF_PROVIDER} | cut -d\  -f 7 | cut -d/ -f 1)
 echo -e "${IF_PROVIDER}: \e[32m${INET_PROVIDER}\e[m"
 INET_MGMT=$(ip -f inet -o addr show ${IF_MGMT} | cut -d\  -f 7 | cut -d/ -f 1)
 echo -e "${IF_MGMT}: \e[32m${INET_MGMT}\e[m"
 
-
+# status check
 func () {
     SERVICE=$1
-    echo "${SERVICE}.service"
     systemctl --quiet is-active ${SERVICE}.service
     if [ $? -eq 0 ]; then
-            echo -e "> \e[32mRunning\e[m"
+        echo -e "${SERVICE}.service: \e[32mactive (running)\e[m"
     else
-            echo -e "> \e[31mDead\e[m"
+        echo -e "${SERVICE}.service: \e[31mfailed\e[m"
     fi
 }
 
+# main
+if [ ${MODE} = "controller" -o ${MODE} = "con" ]; then
+    basic_service=(chrony mysql rabbitmq-server etcd)
+    os_service=(glance-api \
+                   nova-api nova-conductor nova-novncproxy nova-scheduler nova-compute \
+                   neutron-dhcp-agent neutron-l3-agent neutron-linuxbridge-agent \
+                   neutron-linuxbridge-cleanup neutron-metadata-agent \
+                   neutron-server zun-api zun-wsproxy)
+elif [ ${MODE} = "compute" -o ${MODE} = "com" ]; then
+    basic_service=(chrony)
+    os_service=(nova-compute \
+                   docker containerd kuryr-libnetwork \
+                   neutron-linuxbridge-cleanup neutron-linuxbridge-agent \
+                   zun-cni-daemon zun-compute)
+else
+    echo "Usage: ./${PROGRAM} [mode]"
+    echo "  mode: controller, compute"
+    exit 0;
+fi
+
 echo -e "\e[34m===== Daemon Status Check =====\e[m"
-service_array=(chrony mysql rabbitmq-server etcd)
-for i in "${service_array[@]}"
+for i in "${basic_service[@]}"
 do
     func ${i}
 done
 
 echo -e "\e[34m===== Daemon Status Check (OpenStack) =====\e[m"
-service_array=(glance-api \
-               nova-api nova-conductor nova-novncproxy nova-scheduler nova-compute \
-               neutron-dhcp-agent neutron-l3-agent neutron-linuxbridge-agent \
-               neutron-linuxbridge-cleanup neutron-metadata-agent neutron-ovs-cleanup \
-               neutron-server zun-api zun-wsproxy)
-for i in "${service_array[@]}"
+for i in "${os_service[@]}"
 do
     func ${i}
 done
